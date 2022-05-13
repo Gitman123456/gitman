7c51cae9d463118d5e4bfaba82bdd51b610f0599
Gitman123456 <102791144+Gitman123456@users.noreply.github.com>
2022-05-13 16:53:52 +0900

    웹 7주차 게임만들기

---
+++ js/game.js
@@ -0,0 +1,44 @@
+var board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+
+const ME = 1
+const AI = -1
+
+function evalute() {
+
+}
+
+function gameOver() {
+
+}
+
+function gameOverAll() {
+
+}
+
+function emptyCells() {
+
+}
+
+function setMove() {
+
+}
+
+function minmax() {
+
+}
+
+function aiTurn() {
+
+}
+
+function clickedCell() {
+
+}
+
+function restartBtn() {
+
+}
