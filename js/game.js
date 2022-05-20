11dad66fe5f71be0dbe6c8bd59ca5ea415b21d74
Gitman123456 <102791144+Gitman123456@users.noreply.github.com>
2022-05-20 16:54:59 +0900

    틱택토 게임 자바스크립트

--- js/game.js
+++ js/game.js
@@ -11,8 +11,33 @@ function evalute() {
 
 }
 
-function gameOver() {
+function gameOver(state, player) {
+    var win_state = [
+        [state[0][0],state[0][1],state[0][2]],
+        [state[1][0],state[1][1],state[1][2]],
+        [state[2][0],state[2][1],state[2][2]],
+        [state[0][0],state[1][0],state[2][0]],
+        [state[0][1],state[1][1],state[2][1]],
+        [state[0][2],state[1][2],state[2][2]],
+        [state[0][0],state[1][1],state[2][2]],
+        [state[0][2],state[1][1],state[2][0]]
+    ];
 
+    for (var i = 0; i < 8; i++) {
+        var line = win_state[i]
+        var count = 0
+        for (var j = 0; j < 3;j++) {
+            if (line[j] == player) {
+                count++;
+            }
+        }
+        
+        if (count == 3) {
+            return true;
+        }
+    }
+    
+    return false;
 }
 
 function gameOverAll() {
